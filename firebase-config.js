cb3a29bb16d1aa9d2c5babfd96d6cf1815a2b215
Keistedile <keila.stedile@paysagecorpal.com.br>
2026-09-11 16:09:25 -0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -23,4 +23,11 @@ const firebaseConfig = {
 
 // Não é necessário editar nada abaixo desta linha.
 firebase.initializeApp(firebaseConfig);
-const db = firebase.firestore();
+const firebaseConfig = {
+  apiKey: "AIzaSyBR68j3l7DtSU1jWBdlc0SaX2m40lV164U",
+  authDomain: "estoque-paysagecorpal.firebaseapp.com",
+  projectId: "estoque-paysagecorpal",
+  storageBucket: "estoque-paysagecorpal.firebasestorage.app",
+  messagingSenderId: "116609127998",
+  appId: "1:116609127998:web:00031188c0b9a25276236f"
+};
